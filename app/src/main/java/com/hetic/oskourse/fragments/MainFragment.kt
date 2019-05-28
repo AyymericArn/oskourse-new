91ccff0549f9dde1f0fd18eecbf142e1c57ea830
AyymericArn <aymericarnoult@gmail.com>
2019-05-28 18:46:09 +0200

    fragmentation of home ok

--- app/src/main/java/com/hetic/oskourse/fragments/MainFragment.kt
+++ app/src/main/java/com/hetic/oskourse/fragments/MainFragment.kt
@@ -2,23 +2,29 @@ package com.hetic.oskourse.fragments
 
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import com.hetic.oskourse.R
+import com.hetic.oskourse.services.DishWrapper
+import com.hetic.oskourse.services.MealRepository
+import com.hetic.oskourse.viewholder.DishItem
+import com.mikepenz.fastadapter.FastAdapter
+import com.mikepenz.fastadapter.IItem
+import com.mikepenz.fastadapter.adapters.ItemAdapter
+import kotlinx.android.synthetic.main.activity_home.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), TextWatcher {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,5 +34,120 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val thisContext = context
+
+        recyclerView.layoutManager = LinearLayoutManager(
+            thisContext,
+            RecyclerView.VERTICAL,
+            false
+        )
+
+        val itemAdapter = ItemAdapter<IItem<*, *>>()
+        val fastAdapter = FastAdapter.with<DishItem, ItemAdapter<IItem<*, *>>>(itemAdapter)
+
+        fastAdapter.withOnClickListener { view, adapter, item, position ->
+            //            val intent = Intent(this, MealActivity::class.java)
+//            startActivity(intent)
+            true
+        }
+
+        recyclerView.adapter = fastAdapter
+
+        val repository = MealRepository()
+
+        // search bar handler
+
+//        searchBar.addTextChangedListener(object: TextWatcher {
+//            override fun afterTextChanged(str: Editable?) {
+//                val content = str?.text.toString()
+//                str?.error = if (content.length >= 6) null else "Minimum length = 6"
+//            }
+//
+//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+//
+//            override fun onTextChanged(s: Editable?) {
+//                Toast.makeText(this@HomeActivity, s, Toast.LENGTH_SHORT).show()
+//            }
+//        })
+
+        searchBar.addTextChangedListener(this)
+
+        // query to the API
+
+        fun fetchAndDisplay() {
+
+            val search = searchBar.text.toString()
+
+            if (search.isBlank()) {
+                Toast.makeText(thisContext, "is blank", Toast.LENGTH_SHORT).show()
+                for (i in 0..20) {
+                    repository.api.getRandomDish()
+                        .enqueue(object : Callback<DishWrapper> {
+                            override fun onResponse(call: Call<DishWrapper>, response: Response<DishWrapper>) {
+                                val dishWrapper = response.body()
+                                if (dishWrapper != null) {
+
+                                    val res = dishWrapper.meals[0]
+
+                                    val item = DishItem(res)
+
+                                    itemAdapter.add(item)
+                                }
+//                        Toast.makeText(this@HomeActivity, "success", Toast.LENGTH_SHORT).show()
+                            }
+
+                            override fun onFailure(call: Call<DishWrapper>, t: Throwable) {
+                                t.printStackTrace()
+                                Toast.makeText(thisContext, "failure", Toast.LENGTH_SHORT).show()
+                            }
+                        })
+                }
+            } else {
+                repository.api.searchDish(search)
+                    .enqueue(object : Callback<DishWrapper> {
+                        override fun onResponse(call: Call<DishWrapper>, response: Response<DishWrapper>) {
+                            val dishWrapper = response.body()
+                            if (dishWrapper != null) {
+
+                                for(myDish in dishWrapper.meals) {
+                                    val item = DishItem(myDish)
+                                    itemAdapter.add(item)
+                                }
+                            }
+//                        Toast.makeText(this@HomeActivity, "success", Toast.LENGTH_SHORT).show()
+                        }
+
+                        override fun onFailure(call: Call<DishWrapper>, t: Throwable) {
+                            t.printStackTrace()
+                            Toast.makeText(thisContext, "failure", Toast.LENGTH_SHORT).show()
+                        }
+                    })
+            }
+
+        }
+
+        deleteButton.setOnClickListener {
+            itemAdapter.removeRange(0, itemAdapter.adapterItemCount)
+        }
+
+        searchButton.setOnClickListener {
+            fetchAndDisplay()
+        }
+
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        val query = searchBar.text.toString()
+
+//        Toast.makeText(this@HomeActivity, query, Toast.LENGTH_SHORT).show()
+    }
 }
