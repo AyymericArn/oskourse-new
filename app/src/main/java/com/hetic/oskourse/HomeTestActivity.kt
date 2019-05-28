32f79e0bb4188a481781be4932f14623fdb3b940
AyymericArn <aymericarnoult@gmail.com>
2019-05-28 16:08:42 +0200

    fixed androidx bugs

--- app/src/main/java/com/hetic/oskourse/HomeTestActivity.kt
+++ app/src/main/java/com/hetic/oskourse/HomeTestActivity.kt
@@ -1,6 +1,6 @@
 package com.hetic.oskourse
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class HomeTestActivity : AppCompatActivity() {
